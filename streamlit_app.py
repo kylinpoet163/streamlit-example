4d7a2b125e6df2ba05d14514227f1a4b32fe3cdc
kylinpoet163 <128133407+kylinpoet163@users.noreply.github.com>
2023-03-17 17:38:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ with open('unit_names.json', encoding='utf-8',errors="ignore") as f:
     
 # 创建 Streamlit 应用程序
 st.title('单位编码查询')
-unit_code = st.text_input('请输入单位编码：')
+unit_code = st.text_input('请输入单位编码：(回车确认)')
 
 # 查询并显示单位名称
 if unit_code in data:
     unit_name = data[unit_code]
-    st.write(f'单位名称为：\n{unit_code}{unit_name}')
+    st.write(f'单位名称为：  \n{unit_code}{unit_name}')
 else:
     st.write('该单位编码不存在，请重新输入。')
