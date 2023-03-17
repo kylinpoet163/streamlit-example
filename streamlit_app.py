ab3c086bf4d0ae3d524a9b6752e2805768617fce
kylinpoet163 <128133407+kylinpoet163@users.noreply.github.com>
2023-03-17 17:25:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ unit_code = st.text_input('请输入单位编码：')
 
 # 查询并显示单位名称
 if unit_code in data:
-    unit_name = unit_code[unit_code]
+    unit_name = data[unit_code]
     st.write(f'单位名称为：{unit_code}{unit_name}')
 else:
     st.write('该单位编码不存在，请重新输入。')
